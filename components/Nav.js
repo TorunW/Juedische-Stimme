faf8c8cee2de9483ae1ffe50cdffb29267f40d22
david nelband <dnelband@gmail.com>
2022-07-25 19:44:00 +0200

    merge

--- components/Nav.js
+++ components/Nav.js
@@ -4,25 +4,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import styles from 'styles/Nav.module.css';
 
 function Nav() {
-<<<<<<< HEAD
-    const { items } = useSelector(state => state.nav)
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <Link href={"/"}>Home</Link>
-                </li>
-                {items.map((item,index)=> (
-                    <li key={Date.now() + index}>
-                        <Link href={'/'+ (item.link && item.link !== null ? item.link : item.post_name)}>
-                            {item.title && item.title !== null ? item.title : item.post_title}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        </nav>
-    )
-=======
   const { items } = useSelector((state) => state.nav);
 
   return (
@@ -32,15 +13,16 @@ function Nav() {
         <div>Call to action button</div>
       </div>
       <div className={styles.bottomContainer}>
-        {items.map((item, index) => (
-          <div key={index}>
-            <Link href={'/' + item.post_name}>{item.post_title}</Link>
-          </div>
-        ))}
+      {items.map((item,index)=> (
+                    <li key={Date.now() + index}>
+                        <Link href={'/'+ (item.link && item.link !== null ? item.link : item.post_name)}>
+                            {item.title && item.title !== null ? item.title : item.post_title}
+                        </Link>
+                    </li>
+                ))}
       </div>
     </nav>
   );
->>>>>>> f005007e8ac6eb5202ca09b803da970d0b1f9275
 }
 
 export default Nav;
