6761cafb84900e252837ca9609825f0592aee050
TorunW <torun.wikstrom@gmail.com>
2022-07-25 21:31:50 +0200

    started with basic styling of components

--- components/FacebookEvents.js
+++ components/FacebookEvents.js
@@ -1,83 +1,95 @@
-import React, {useEffect } from 'react'
-import dateTimeHelper from 'helpers/dateTimeHelper'
-import axios from 'axios'
+import React, { useEffect } from 'react';
+import dateTimeHelper from 'helpers/dateTimeHelper';
+import axios from 'axios';
 import renderToString from 'helpers/renderToString';
 
-import { setEvents  } from 'store/fbdata/fbDataSlice'
-import { useDispatch, useSelector } from 'react-redux'
+import { setEvents } from 'store/fbdata/fbDataSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 const FacebookEvents = (props) => {
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.fbData.token);
+  const events = useSelector((state) => state.fbData.events);
 
-    const dispatch = useDispatch();
-    const token = useSelector(state => state.fbData.token)
-    const events = useSelector(state => state.fbData.events);
+  useEffect(() => {
+    if (token) initFacebookEvents();
+  }, [token]);
 
-    useEffect(() => {
-        if (token) initFacebookEvents()
-    }, [token])
-    
-    async function initFacebookEvents(){
-        if (!events){
-            fetchFacebookEvents()
-        } else {
-            const fbEventsUpdatedMonth = parseInt(events.date_updated.split('-')[1]);
-            const fbEventsUpdatedDay = parseInt(events.date_updated.split('-')[2])
-            const today = new Date();
-            const month = today.getMonth() + 1;
-            const day = today.getDate();
-            if (day !== fbEventsUpdatedDay || month !== fbEventsUpdatedMonth) fetchFacebookEvents()
+  async function initFacebookEvents() {
+    if (!events) {
+      fetchFacebookEvents();
+    } else {
+      const fbEventsUpdatedMonth = parseInt(events.date_updated.split('-')[1]);
+      const fbEventsUpdatedDay = parseInt(events.date_updated.split('-')[2]);
+      const today = new Date();
+      const month = today.getMonth() + 1;
+      const day = today.getDate();
+      if (day !== fbEventsUpdatedDay || month !== fbEventsUpdatedMonth)
+        fetchFacebookEvents();
+    }
+  }
+
+  async function fetchFacebookEvents() {
+    const res = await fetch(
+      `https://graph.facebook.com/998665673528998/events?limit=3&access_token=${token}`
+    );
+    const fetchedEvents = await res.json();
+    // remove all the weird characters from the content to avoid mySql errors
+    if (fetchedEvents.data && fetchedEvents.data.length > 0) {
+      const renderedEvents = renderToString(fetchedEvents.data);
+      axios({
+        method: 'post',
+        url: `/api/fbfeed`,
+        data: {
+          content: renderedEvents,
+          date_updated: dateTimeHelper(new Date()),
+          type: 'events',
+        },
+      }).then(
+        (response) => {
+          console.log(fetchedEvents, ' FETCHED EVENTS');
+          dispatch(setEvents(fetchedEvents.data));
+          // setEvents(fetchedEvents.data)
+          console.log(response, 'response on create fb feed record');
+          // window.location.href = "/admin/posts/page/1" // BETTER FETCH THE POSTS THEN REFRESH PAGE
+        },
+        (error) => {
+          console.log(error, 'ERROR on create fb feed record');
+          // console.log('NOW NEEDS TO DISPLAY ERRORS!')
         }
+      );
     }
+  }
 
-    async function fetchFacebookEvents(){
-        const res  = await fetch(`https://graph.facebook.com/998665673528998/events?limit=3&access_token=${token}`)
-        const fetchedEvents = await res.json()
-        // remove all the weird characters from the content to avoid mySql errors
-        if (fetchedEvents.data && fetchedEvents.data.length > 0){
-            const renderedEvents = renderToString(fetchedEvents.data)
-            axios({
-                method: 'post',
-                url: `/api/fbfeed`,
-                data: {
-                    content:renderedEvents,
-                    date_updated:dateTimeHelper(new Date()),
-                    type:'events'
-                }
-            }).then((response) => {
-                console.log(fetchedEvents, " FETCHED EVENTS")
-                dispatch(setEvents(fetchedEvents.data))
-                // setEvents(fetchedEvents.data)
-                console.log(response,"response on create fb feed record");
-                // window.location.href = "/admin/posts/page/1" // BETTER FETCH THE POSTS THEN REFRESH PAGE
-            }, (error) => {
-                console.log(error, "ERROR on create fb feed record");
-                // console.log('NOW NEEDS TO DISPLAY ERRORS!')
-            });
-        }
-    }
+  let eventsDisplay;
+  if (events && events.content && events.content.length > 0) {
+    const eventsArray = JSON.parse(events.content);
+    console.log(eventsArray, ' EVENTS ARRAY ');
+    eventsDisplay = eventsArray.map((fbEvent, index) => {
+      if (index <= 2) {
+        return (
+          <div
+            key={index}
+            style={{ width: '33%', float: 'left', padding: '5px' }}
+          >
+            <h2>{fbEvent.name}</h2>
+            <p>
+              {fbEvent.description.length > 300
+                ? `${fbEvent.description.substring(0, 300)}[...]`
+                : fbEvent.description}
+            </p>
+          </div>
+        );
+      }
+    });
+  }
 
-    let eventsDisplay;
-    if (events && events.content && events.content.length > 0){
-        const eventsArray = JSON.parse(events.content);
-        console.log(eventsArray, " EVENTS ARRAY ")
-        eventsDisplay = eventsArray.map((fbEvent, index) => {
-            if (index <= 2){
-                return (
-                    <div key={index} style={{width: "33%", float: "left",padding:"5px"}}>
-                        <h2>{fbEvent.name}</h2>
-                        <p>{fbEvent.description}</p>
-                    </div>
-                )
-            }
-        })
-    }
+  return (
+    <div id='events' style={{ overflow: 'auto', backgroundColor: '#efefef' }}>
+      <h1>FACEBOOK EVENTS</h1>
+      <div>{eventsDisplay}</div>
+    </div>
+  );
+};
 
-    return (
-        <div id="events" style={{overflow:"auto", backgroundColor: "#efefef"}}>
-            <h1>FACEBOOK EVENTS</h1>
-            <div>{eventsDisplay}</div>
-        </div>
-    )
-}
-
-export default FacebookEvents
+export default FacebookEvents;
--- components/Posts.js
+++ components/Posts.js
@@ -3,49 +3,54 @@ import styles from 'styles/Articles.module.css';
 
 function Posts(props) {
   return (
-    <div className={styles.articles}>
-      {props.posts.map((post, index) => {
-        let content = post.post_content;
-        const textLength = 600;
-        let startIndex = 0,
-          endIndex = textLength;
+    <div className={styles.articlesPage}>
+      <h1>Aktuelles</h1>
+      <div className={styles.articleContainer}>
+        {props.posts.map((post, index) => {
+          let content = post.post_content;
+          const textLength = 600;
+          let startIndex = 0,
+            endIndex = textLength;
 
-        // if we have a phrase - search phrase, i.e if this is search page, we will search for the phrase inside the content
-        if (props.phrase) {
-          content = content.replace(/<\/?[^>]+(>|$)/g, '');
+          // if we have a phrase - search phrase, i.e if this is search page, we will search for the phrase inside the content
+          if (props.phrase) {
+            content = content.replace(/<\/?[^>]+(>|$)/g, '');
 
-          const phraseIndexInText = post.post_content.indexOf(props.phrase);
+            const phraseIndexInText = post.post_content.indexOf(props.phrase);
 
-          if (phraseIndexInText > -1) {
-            startIndex = phraseIndexInText - textLength / 2;
-            if (startIndex < 0) startIndex = 0;
+            if (phraseIndexInText > -1) {
+              startIndex = phraseIndexInText - textLength / 2;
+              if (startIndex < 0) startIndex = 0;
 
-            endIndex = phraseIndexInText + textLength / 2;
-            if (endIndex > post.post_content.length - 1)
-              endIndex = post.post_content.length;
+              endIndex = phraseIndexInText + textLength / 2;
+              if (endIndex > post.post_content.length - 1)
+                endIndex = post.post_content.length;
 
-            content =
-              '...' +
-              content
-                .toLowerCase()
-                .split(props.phrase)
-                .join(`<b>${props.phrase}</b>`) +
-              '...';
-          }
-        } else content = content.substring(startIndex, endIndex);
+              content =
+                '...' +
+                content
+                  .toLowerCase()
+                  .split(props.phrase)
+                  .join(`<b>${props.phrase}</b>`) +
+                '...';
+            }
+          } else content = content.substring(startIndex, endIndex);
 
-        return (
-          <article key={index} className={styles.articleContainer}>
-            <h2>
-              <a href={'/' + post.post_name}>{post.post_title}</a>
-            </h2>
-            <h4>
-              <a href={`/category/${post.categoryName}`}>{post.categoryName}</a>
-            </h4>
-            <div dangerouslySetInnerHTML={{ __html: content }}></div>
-          </article>
-        );
-      })}
+          return (
+            <article key={index} className={styles.article}>
+              <h2>
+                <a href={'/' + post.post_name}>{post.post_title}</a>
+              </h2>
+              <h4>
+                <a href={`/category/${post.categoryName}`}>
+                  {post.categoryName}
+                </a>
+              </h4>
+              <div dangerouslySetInnerHTML={{ __html: content }}></div>
+            </article>
+          );
+        })}
+      </div>
     </div>
   );
 }
