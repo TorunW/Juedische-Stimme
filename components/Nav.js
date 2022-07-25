a8392bc8af4e2bdb174bfbd74afd4578935ae93e
TorunW <torun.wikstrom@gmail.com>
2022-07-25 18:19:27 +0200

    Started to implement basic css

--- components/Nav.js
+++ components/Nav.js
@@ -1,23 +1,26 @@
-import React, { useEffect, useState } from 'react'
-import Link from 'next/link'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { useDispatch, useSelector } from 'react-redux';
+import styles from 'styles/Nav.module.css';
 
 function Nav() {
-    const { items } = useSelector(state => state.nav)
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <Link href={"/"}>Home</Link>
-                </li>
-                {items.map((item,index)=>(
-                    <li key={index}>
-                        <Link href={'/'+item.post_name}>{item.post_title}</Link>
-                    </li>
-                ))}
-            </ul>
-        </nav>
-    )
+  const { items } = useSelector((state) => state.nav);
+
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.topContainer}>
+        <Link href={'/'}>Home</Link>
+        <div>Call to action button</div>
+      </div>
+      <div className={styles.bottomContainer}>
+        {items.map((item, index) => (
+          <div key={index}>
+            <Link href={'/' + item.post_name}>{item.post_title}</Link>
+          </div>
+        ))}
+      </div>
+    </nav>
+  );
 }
 
-export default Nav
+export default Nav;
