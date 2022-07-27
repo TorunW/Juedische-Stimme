60ccdd78e071cfff33ca8bf4fa70cf4f9282075d
TorunW <torun.wikstrom@gmail.com>
2022-07-27 18:20:44 +0200

    added slideshow functionality to the gallery

--- components/AboutInfo.tsx
+++ components/AboutInfo.tsx
@@ -1,31 +1,39 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement } from 'react';
 import { useDispatch, useSelector } from 'store/hooks';
 import Gallery from './Gallery';
+import styles from 'styles/About.module.css';
 
 const AboutInfo = () => {
+  const { gallery } = useSelector((state) => state.aboutinfo);
+  const { aboutInfo } = useSelector((state) => state.aboutinfo);
 
-  const { gallery } = useSelector((state) => state.aboutinfo)
-  const { aboutInfo } = useSelector((state) => state.aboutinfo)
-
-  let aboutInfoDisplay : ReactElement;
-  if (aboutInfo !== null){
+  let aboutInfoDisplay: ReactElement;
+  if (aboutInfo !== null) {
     aboutInfoDisplay = (
-      <div>
-        <div dangerouslySetInnerHTML={{__html:aboutInfo.text_top}}></div>
-        <Gallery 
-          gallery={gallery}
-        />
-        <div dangerouslySetInnerHTML={{__html:aboutInfo.text_bottom}}></div>
+      <div className={styles.aboutContainer}>
+        <div dangerouslySetInnerHTML={{ __html: aboutInfo.text_top }}></div>
+        <Gallery gallery={gallery} />
+        <div dangerouslySetInnerHTML={{ __html: aboutInfo.text_bottom }}></div>
       </div>
-    )
+    );
   }
 
   return (
-    <div id="about-info">
-        <h1>Über Uns</h1>
-        {aboutInfoDisplay}
+    <div id='about-info' className={styles.aboutPage}>
+      <h1>Über Uns</h1>
+      {aboutInfoDisplay}
+      <div className={styles.contentContainer}>
+        <img src='about.jpg' />
+        <div className={styles.linkContainer}>
+          <p>Grundlagen unserer Arbeit sind</p>
+          <div className={styles.btnContainer}>
+            <button>Selbsverständnis der Jüdischen Stimme</button>
+            <button>Satzung der Jüdischen Stimme</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutInfo
+export default AboutInfo;
--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,15 +1,59 @@
-import React from 'react'
-import { generateImageUrl } from 'helpers/imageUrlHelper'
+import React, { useState } from 'react';
+import { generateImageUrl } from 'helpers/imageUrlHelper';
+import styles from 'styles/Gallery.module.css';
 
-const Gallery = ({gallery}) => {
-    const images = gallery.imageSrcs.split(',').map((imgSrc:string,index:number) => (
+const Gallery = ({ gallery }) => {
+  const [slideIndex, setSlideIndex] = useState(1);
+
+  function nextSlide() {
+    if (slideIndex !== slideShow.length) {
+      setSlideIndex(slideIndex + 1);
+    } else if (slideIndex === slideShow.length) {
+      setSlideIndex(1);
+    }
+  }
+
+  function prevSlide() {
+    if (slideIndex !== 1) {
+      setSlideIndex(slideIndex - 1);
+    } else if (slideIndex === 1) {
+      setSlideIndex(slideShow.length);
+    }
+  }
+
+  let slideShow = gallery.imageSrcs
+    .split(',')
+    .map((imgSrc: string, index: number) => (
+      <div
+        className={slideIndex === index + 1 ? styles.slideActive : styles.slide}
+      >
         <img key={Date.now() + index} src={generateImageUrl(imgSrc)} />
-    ))
-    return (
-        <div>
-            {images}
-        </div>
-    )
-}
+      </div>
+    ));
 
-export default Gallery
+  console.log(slideShow.length);
+
+  // = gallery.imageSrcs
+  // .split(',')
+  // .map((imgSrc: string, index: number) => (
+  //   <div className={styles.item}>
+  //     <img key={Date.now() + index} src={generateImageUrl(imgSrc)} />
+  //   </div>
+  // ));
+
+  return (
+    <div>
+      <div className={styles.containerSlider}>
+        <button className={styles.prev} onClick={prevSlide}>
+          Prev
+        </button>
+        {slideShow}
+        <button className={styles.next} onClick={nextSlide}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
