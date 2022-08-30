21f1462350c98ba9bc4f36bea21d00306c328091
david nelband <dnelband@gmail.com>
2022-08-30 20:37:24 +0200

    added first + last to pagination

--- components/pagination/Pagination.tsx
+++ components/pagination/Pagination.tsx
@@ -4,14 +4,20 @@ import styles from './Styles.module.css';
 
 const Pagination = ({pageNum,itemsCount,itemsPerPage,type,title}) => {
 
+    const totalPages = Math.ceil(itemsCount / itemsPerPage)
+
     const { minNumber, maxNumber } = getPaginationRange(
         pageNum,
-        Math.ceil(itemsCount / itemsPerPage)
+        totalPages
     );
 
     const pages = [];
     for (let i = 1; i <= Math.ceil(itemsCount / itemsPerPage); i++) {
         pages.push(i);
+    }
+
+    function onFirstPageClick(){
+        window.location.href = `/${type}/${title}/page/1`;
     }
 
     function onPrevPageClick() {
@@ -22,9 +28,15 @@ const Pagination = ({pageNum,itemsCount,itemsPerPage,type,title}) => {
         window.location.href = `/${type}/${title}/page/${pageNum + 1}`;
     }
 
+    function onLastPageClick() {
+        window.location.href = `/${type}/${title}/page/${totalPages - 1}`;
+    }
+
+
     return (
         <div>
             <ul className={styles.pagination}>
+                <button onClick={onFirstPageClick}>first</button>
                 <button onClick={onPrevPageClick}>prev</button>
                 {pages
                 .filter((number) => number >= minNumber && number < maxNumber)
@@ -39,6 +51,7 @@ const Pagination = ({pageNum,itemsCount,itemsPerPage,type,title}) => {
                     </li>
                 ))}
                 <button onClick={onNextPageClick}>next</button>
+                <button onClick={onLastPageClick}>last</button>
             </ul>
         </div>
     )
