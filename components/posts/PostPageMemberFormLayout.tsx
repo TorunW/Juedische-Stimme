633d5814d17eb66033985a3cf8576192c1882a49
david nelband <dnelband@gmail.com>
2022-09-11 12:59:38 +0200

    posts refractoring

--- components/posts/PostPageMemberFormLayout.tsx
+++ components/posts/PostPageMemberFormLayout.tsx
@@ -1,38 +1,17 @@
 import Image from 'next/image';
 import React from 'react';
 import styles from './ListStyles.module.css';
-import background from '../../styles/images/memberspage-bg.jpg';
+import background from 'styles/images/memberspage-bg.jpg';
 import MembershipForm from '../forms/MembershipForm';
+import { generateImageUrl } from 'helpers/imageUrlHelper';
+import { getPostContentFields } from 'helpers/getPostContentFields';
 
 const PostPageMemberFormLayout = ({ post, locale }) => {
-  let postTitle = post.post_title,
-    postExcerpt = post.post_excerpt,
-    postExcerpt2 = post.post_excerpt_2,
-    postContent = post.post_content,
-    postContent2 = post.post_content_2;
-
-  if (locale !== null) {
-    postTitle = post[`post_title_translation_${locale}`]
-      ? post[`post_title_translation_${locale}`]
-      : post.post_title;
-    postExcerpt = post[`post_excerpt_translation_${locale}`]
-      ? post[`post_excerpt_translation_${locale}`]
-      : post.post_excerpt;
-    postExcerpt2 = post[`post_excerpt_2_translation_${locale}`]
-      ? post[`post_excerpt_2_translation_${locale}`]
-      : post.post_excerpt;
-    postContent = post[`post_content_translation_${locale}`]
-      ? post[`post_content_translation_${locale}`]
-      : post.post_content;
-    postContent2 = post[`post_content_2_translation_${locale}`]
-      ? post[`post_content_2_translation_${locale}`]
-      : post.post_content;
-  }
-
+  const { postTitle, postExcerpt, postExcerpt2, postContent, postContent2 } = getPostContentFields(post, locale)
   return (
     <React.Fragment>
       <div className={styles.memberFormLayout}>
-        <Image src={background} layout='fill' objectFit='cover' />
+        <Image src={generateImageUrl(post.post_image)} layout='fill' objectFit='cover' />
 
         <div className={styles.contentContainer}>
           <h2>Mitgliedsantrag</h2>
