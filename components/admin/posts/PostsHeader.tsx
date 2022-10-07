1d5e20df5b1d314c2cca402f4c3d816128f6ab87
TorunW <torun.wikstrom@gmail.com>
2022-10-07 20:43:18 +0200

    tabs for post list

--- components/admin/posts/PostsHeader.tsx
+++ components/admin/posts/PostsHeader.tsx
@@ -1,30 +1,49 @@
-import React from 'react'
-import { useSelector } from 'store/hooks'
-
+import React from 'react';
+import { useSelector } from 'store/hooks';
+import {
+  AppBar,
+  Box,
+  Button,
+  Card,
+  InputLabel,
+  Select,
+  MenuItem,
+  FormControl,
+  Tab,
+  Tabs,
+} from '@mui/material';
 const PostsHeader = () => {
+  const { categories, categoryName } = useSelector((state) => state.categories);
 
-    const { categories, categoryName } = useSelector(state => state.categories)
+  console.log(categories, categoryName);
 
-    console.log(categories, categoryName)
+  return (
+    <Box
+      id='admin-posts-header'
+      sx={{ borderBottom: 1, borderColor: 'divider' }}
+    >
+      <Tabs
+        value={categoryName}
+        onClick={(e) =>
+          (window.location.href = `/admin/posts/category/${e.target.value}/page/1`)
+        }
+        textColor='secondary'
+        indicatorColor='secondary'
+      >
+        {categories && categories !== null
+          ? categories.map((category, index) => (
+              <Tab
+                key={category.term_id}
+                value={category.name}
+                label={category.name}
+              >
+                {category.name}
+              </Tab>
+            ))
+          : ''}
+      </Tabs>
+    </Box>
+  );
+};
 
-    return (
-        <div id="admin-posts-header">
-            <div>
-                <label>SELECT CATEGORY</label>
-                <select value={categoryName} onChange={e => window.location.href = `/admin/posts/category/${e.target.value}/page/1`}>
-                    {categories && categories !== null 
-                        ?
-                            categories.map((category,index)=>(
-                                <option key={category.term_id} value={category.name}>{category.name}</option>
-                            ))
-                        :
-                            ""
-                    }
-                </select>
-            </div>
-            <hr/>
-        </div>
-    )
-}
-
-export default PostsHeader
+export default PostsHeader;
