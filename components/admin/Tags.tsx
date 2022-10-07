beb77c6d3fd98f0e7fc002b02f1940401e6bdba9
TorunW <torun.wikstrom@gmail.com>
2022-10-07 17:11:54 +0200

    Tag form styled, form helper added

--- components/admin/Tags.tsx
+++ components/admin/Tags.tsx
@@ -1,45 +1,43 @@
-import React, { FC } from 'react'
+import React, { FC } from 'react';
 import axios from 'axios';
 import { Tag } from 'types/Tag.type';
 
 interface AdminTagsProps {
-    tags?: Tag[]
+  tags?: Tag[];
 }
 
-const AdminTags: FC<AdminTagsProps> = ({tags}) => {
+const AdminTags: FC<AdminTagsProps> = ({ tags }) => {
+  function onDeleteTag(tag: Tag) {
+    // console.log('now delete the tag + ALL OF ITS RELATIONSHIP, TAXONOMY USW')
+    axios.delete(`/api/tags/tag/${tag.term_id}`).then(
+      (response) => {
+        window.location.reload();
+        console.log(response, 'response on delete tag');
+        console.log('NOW NEEDS TO REFRESH TAGS LIST!');
+      },
+      (error) => {
+        console.log(error, 'ERROR on delete tag');
+        console.log('NOW NEEDS TO DISPLAY ERROR');
+      }
+    );
+  }
 
-    function onDeleteTag(tag:Tag){
-        // console.log('now delete the tag + ALL OF ITS RELATIONSHIP, TAXONOMY USW')
-        axios.delete(`/api/tags/tag/${tag.term_id}`).then((response) => {
-            window.location.reload()
-            console.log(response,"response on delete tag");
-            console.log('NOW NEEDS TO REFRESH TAGS LIST!');
-        }, (error) => {
-            console.log(error, "ERROR on delete tag");
-            console.log('NOW NEEDS TO DISPLAY ERROR')
-        });
-    }
+  return (
+    <div>
+      {tags.map((tag, index) => (
+        <li key={tag.term_id}>
+          <h3>
+            {tag.name} <small>({tag.count})</small>
+          </h3>
+          <a href={`/admin/tags/${tag.term_id}`}>edit</a>
+          <br />
+          <a href={`/tag/${tag.slug}/page/1`}>view on live site</a>
+          <br />
+          <a onClick={() => onDeleteTag(tag)}>DELETE TAG</a>
+        </li>
+      ))}
+    </div>
+  );
+};
 
-    return (
-        <div>
-            {tags.map((tag,index) => (
-                <li key={tag.term_id}>
-                    <h3>{tag.name} <small>({tag.count})</small></h3>
-                    <a href={`/admin/tags/${tag.term_id}`}>
-                        edit
-                    </a>
-                    <br/>
-                    <a href={`/tag/${tag.slug}/page/1`}>
-                        view on live site
-                    </a>
-                    <br/>
-                    <a onClick={() => onDeleteTag(tag)}>
-                        DELETE TAG
-                    </a>
-                </li>
-            ))}
-        </div>
-    )
-}
-
-export default AdminTags
+export default AdminTags;
---
+++ components/admin/posts/FormHelp.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import Grid from '@mui/material/Grid';
+import { Typography, Popover, Box, IconButton } from '@mui/material';
+import HelpOutline from '@mui/icons-material/HelpOutline';
+
+interface Props {
+  text: string;
+}
+
+const FormHelp: React.FC<Props> = ({ text }) => {
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
+    null
+  );
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
+  return (
+    <Box>
+      <IconButton aria-describedby={id} onClick={handleClick} color='secondary'>
+        <HelpOutline color='secondary' />
+      </IconButton>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'center',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'center',
+          horizontal: 'right',
+        }}
+        sx={{ marginLeft: 20, marginRight: 5 }}
+      >
+        <Typography sx={{ margin: 3 }}>{text}</Typography>
+      </Popover>
+    </Box>
+  );
+};
+
+export default FormHelp;
