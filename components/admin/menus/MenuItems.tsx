5d37e7b625007486c42e878b5eb07c8f23eb0114
TorunW <torun.wikstrom@gmail.com>
2022-10-14 18:52:53 +0200

    added tabs

--- components/admin/menus/MenuItems.tsx
+++ components/admin/menus/MenuItems.tsx
@@ -2,8 +2,6 @@ import React, { ReactElement, useState } from 'react';
 import axios from 'axios';
 import menuTypes from 'lib/menuTypes.json';
 import {
-  Box,
-  TableContainer,
   Table,
   TableHead,
   TableRow,
@@ -11,7 +9,6 @@ import {
   TableBody,
   Paper,
   Link,
-  Divider,
   IconButton,
   Button,
   Dialog,
@@ -20,9 +17,11 @@ import {
   DialogTitle,
   DialogActions,
   Tabs,
+  Tab,
 } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import AdminTopBar from '@/components/atoms/AdminTopBar';
 
 const MenuItems = ({ menuItems }) => {
   const [currentMenu, setCurrentMenu] = useState('main_menu');
@@ -62,63 +61,91 @@ const MenuItems = ({ menuItems }) => {
     if (menuItemsArray.length === 0)
       menuItemsDisplay = <p>no menu items for {currentMenu}</p>;
     else {
-      menuItemsDisplay = menuItemsArray.map((menuItem: any, index: number) => (
-        <TableBody
-          key={index}
-          sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-        >
-          <TableRow hover role='checkbox' tabIndex={-1}>
-            <TableCell>
-              <Link href={`/admin/menus/${menuItem.term_id}`}>
-                {menuItem.title}
-              </Link>
-            </TableCell>
-            <TableCell align='right'>{menuItem.term_order}</TableCell>
-            <TableCell align='right'>{menuItem.term_order}</TableCell>
-            <TableCell align='right'>{menuItem.link}</TableCell>
-            <TableCell align='right'>{menuItem.taxonomy}</TableCell>
-            <TableCell align='right'>
-              <IconButton>
+      menuItemsDisplay = menuItemsArray.map((menuItem: any, index: number) => {
+        console.log(menuItem);
+        return (
+          <TableBody
+            key={index}
+            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+          >
+            <TableRow hover role='checkbox' tabIndex={-1}>
+              <TableCell>
                 <Link href={`/admin/menus/${menuItem.term_id}`}>
-                  <EditIcon />
+                  {menuItem.title}
                 </Link>
-              </IconButton>
-            </TableCell>
+              </TableCell>
+              <TableCell align='right'>{menuItem.term_order}</TableCell>
+              <TableCell align='right'>{menuItem.link}</TableCell>
+              <TableCell align='right'>{menuItem.taxonomy}</TableCell>
+              <TableCell align='right'>
+                <Link href={`/admin/menus/${menuItem.term_id}`}>
+                  <IconButton>
+                    <EditIcon />
+                  </IconButton>
+                </Link>
+              </TableCell>
 
-            <TableCell align='right'>
-              <IconButton onClick={handleClickOpen}>
-                <DeleteIcon />
-              </IconButton>
+              <TableCell align='right'>
+                <IconButton onClick={handleClickOpen}>
+                  <DeleteIcon />
+                </IconButton>
 
-              <Dialog open={open} onClose={handleClose}>
-                <DialogTitle>{'Delete Tag?'}</DialogTitle>
-                <DialogContent>
-                  <DialogContentText>
-                    {` Once the tag is delete it can't be retrived again`}
-                  </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                  <Button
-                    variant='outlined'
-                    onClick={() => deleteMenuItem(menuItem)}
-                    autoFocus
-                  >
-                    Delete
-                  </Button>
-                  <Button variant='outlined' onClick={handleClose}>
-                    Cancel
-                  </Button>
-                </DialogActions>
-              </Dialog>
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      ));
+                <Dialog open={open} onClose={handleClose}>
+                  <DialogTitle>{'Delete Tag?'}</DialogTitle>
+                  <DialogContent>
+                    <DialogContentText>
+                      {` Once the tag is delete it can't be retrived again`}
+                    </DialogContentText>
+                  </DialogContent>
+                  <DialogActions>
+                    <Button
+                      variant='outlined'
+                      onClick={() => deleteMenuItem(menuItem)}
+                      autoFocus
+                    >
+                      Delete
+                    </Button>
+                    <Button variant='outlined' onClick={handleClose}>
+                      Cancel
+                    </Button>
+                  </DialogActions>
+                </Dialog>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        );
+      });
     }
   }
 
+  let tabsDisplay = (
+    <Tabs
+      value={currentMenu}
+      onChange={(value, newValue) => {
+        setCurrentMenu(newValue);
+      }}
+      indicatorColor='secondary'
+      TabIndicatorProps={{
+        style: {
+          height: '4px',
+          color: 'white !important',
+        },
+      }}
+    >
+      {menuTypes.map((menu, index) => (
+        <Tab
+          key={menu}
+          label={menu.split('_').join(' ')}
+          value={menu}
+          sx={{ color: 'white !important' }}
+        />
+      ))}
+    </Tabs>
+  );
+
   return (
-    <div>
+    <>
+      <AdminTopBar tabs={tabsDisplay} />
       <Paper sx={{ width: '100%', overflow: 'hidden' }}>
         <Table sx={{ minWidth: 650 }} stickyHeader>
           <TableHead>
@@ -134,14 +161,7 @@ const MenuItems = ({ menuItems }) => {
           {menuItemsDisplay}
         </Table>
       </Paper>
-      <ul>
-        {menuTypes.map((menu, index) => (
-          <li key={index + Date.now()}>
-            <a onClick={() => setCurrentMenu(menu)}>{menu}</a>
-          </li>
-        ))}
-      </ul>
-    </div>
+    </>
   );
 };
 
