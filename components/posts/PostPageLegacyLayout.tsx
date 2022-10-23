e402635f692a0803a438b2b3e95ceab692f7afcf
dnelband <dnelband@gmail.com>
2022-10-23 14:51:28 +0200

    imporoved legacy fallback

--- components/posts/PostPageLegacyLayout.tsx
+++ components/posts/PostPageLegacyLayout.tsx
@@ -1,24 +1,21 @@
-import React, { ReactElement } from 'react';
-import styles from '../posts/ListStyles.module.css';
-import formateDate from 'helpers/formateDate';
-import { generateImageUrl } from 'helpers/imageUrlHelper';
-import Image from 'next/image';
-import { getPostContentFields } from 'helpers/getPostContentFields';
-import PostPageEmbededContent from './PostPageEmbededContent';
+import React, { ReactElement } from "react";
+import styles from "../posts/ListStyles.module.css";
+import formateDate from "helpers/formateDate";
+import { generateImageUrl } from "helpers/imageUrlHelper";
+import Image from "next/image";
+import { getPostContentFields } from "helpers/getPostContentFields";
+import PostPageEmbededContent from "./PostPageEmbededContent";
 
 export default function PostPageLegacyLayout({ post, locale }) {
-    const { postTitle, postContent  } =
-    getPostContentFields(post, locale);
-
-
+  const { postTitle, postContent } = getPostContentFields(post, locale);
 
   let tagsDisplay: ReactElement[];
   if (post.tagNames && post.tagNames.length > 0) {
     let tagsArray = [post.tagNames];
-    if (post.tagNames.indexOf(',') > -1) tagsArray = post.tagNames.split(',');
+    if (post.tagNames.indexOf(",") > -1) tagsArray = post.tagNames.split(",");
     tagsDisplay = tagsArray.map((tag, index) => (
-      <a key={index} href={'/tag/' + tag}>
-        {' #' + tag}
+      <a key={index} href={"/tag/" + tag}>
+        {" #" + tag}
       </a>
     ));
   }
@@ -31,31 +28,33 @@ export default function PostPageLegacyLayout({ post, locale }) {
             <h2>{postTitle}</h2>
           </div>
           <div className={styles.linksContainer}>
-          <p>Published {post.post_date ? formateDate(post.post_date) : ''}</p>
-          <p>
-            <a href={`/category/${post.categoryName}`}>#{post.categoryName}</a>
-            {tagsDisplay}
-          </p>
-          <div className={styles.socialMediaLinks}></div>
-        </div>
+            <p>Published {post.post_date ? formateDate(post.post_date) : ""}</p>
+            <p>
+              <a href={`/category/${post.categoryName}`}>
+                #{post.categoryName}
+              </a>
+              {tagsDisplay}
+            </p>
+            <div className={styles.socialMediaLinks}></div>
+          </div>
         </div>
 
         <div className={styles.contentContainer}>
           <div className={styles.topWrapper}>
             <div
-              className={styles.topContent + ' ' + styles.content}
+              className={styles.topContent + " " + styles.content}
               dangerouslySetInnerHTML={{
-                __html: postContent.replace(/(?:\r\n|\r|\n)/g, '<br>'),
+                __html: postContent.replace(/(?:\r\n|\r|\n)/g, "<br>"),
               }}
             ></div>
           </div>
-          {post.post_embed_script && post.post_embed_script !== "null"  ? (
+          {post.post_embed_script && post.post_embed_script !== "null" ? (
             <PostPageEmbededContent
               script={post.post_embed_script}
               html={post.post_embed_html}
             />
           ) : (
-            ''
+            ""
           )}
         </div>
       </div>
--- helpers/getPostLayout.ts
+++ helpers/getPostLayout.ts
@@ -1,5 +1,5 @@
 export function getPostLaoyut(post){
-    let layout = 'article';
+    let layout = 'legacy';
     if (post.post_layout) layout = post.post_layout;
     if (post.categoryName === 'Newsletter') layout = 'newsletter'
     return layout;
