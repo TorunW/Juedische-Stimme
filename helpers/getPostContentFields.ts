02fd09b9fa78969825a29c511ef38c05084bd839
david nelband <torun.wikstrom@gmail.com>
2022-10-23 22:46:51 +0200

    excerpt & content

--- helpers/getPostContentFields.ts
+++ helpers/getPostContentFields.ts
@@ -1,28 +1,27 @@
 export const getPostContentFields = (post, locale) => {
-    let postTitle = post.post_title,
-        postExcerpt = post.post_excerpt,
-        postExcerpt2 = post.post_excerpt_2,
-        postContent = post.post_content,
-        postContent2 = post.post_content_2;
+  let postTitle = post.post_title,
+    postExcerpt = post.post_excerpt,
+    postExcerpt2 = post.post_excerpt_2,
+    postContent = post.post_content,
+    postContent2 = post.post_content_2;
 
-    if (locale !== null) {
+  if (locale !== null) {
     postTitle = post[`post_title_translation_${locale}`]
-        ? post[`post_title_translation_${locale}`]
-        : post.post_title;
+      ? post[`post_title_translation_${locale}`]
+      : post.post_title;
     postExcerpt = post[`post_excerpt_translation_${locale}`]
-        ? post[`post_excerpt_translation_${locale}`]
-        : post.post_excerpt;
+      ? post[`post_excerpt_translation_${locale}`]
+      : post.post_excerpt;
     postExcerpt2 = post[`post_excerpt_2_translation_${locale}`]
-        ? post[`post_excerpt_2_translation_${locale}`]
-        : post.post_excerpt;
+      ? post[`post_excerpt_2_translation_${locale}`]
+      : post.post_excerpt_2;
     postContent = post[`post_content_translation_${locale}`]
-        ? post[`post_content_translation_${locale}`]
-        : post.post_content;
+      ? post[`post_content_translation_${locale}`]
+      : post.post_content;
     postContent2 = post[`post_content_2_translation_${locale}`]
-        ? post[`post_content_2_translation_${locale}`]
-        : post.post_content;
-    }
+      ? post[`post_content_2_translation_${locale}`]
+      : post.post_content_2;
+  }
 
-    return { postTitle, postExcerpt, postExcerpt2, postContent, postContent2 }
-
-}
+  return { postTitle, postExcerpt, postExcerpt2, postContent, postContent2 };
+};
