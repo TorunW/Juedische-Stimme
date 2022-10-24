cf43bebed75bc44001df753b6a9d749f08d38689
david nelband <torun.wikstrom@gmail.com>
2022-10-24 12:51:19 +0200

    added image to legacy

--- components/posts/PostPageLegacyLayout.tsx
+++ components/posts/PostPageLegacyLayout.tsx
@@ -1,12 +1,12 @@
-import React, { ReactElement } from 'react';
-import styles from '../posts/ListStyles.module.css';
-import formateDate from 'helpers/formateDate';
-import { generateImageUrl } from 'helpers/imageUrlHelper';
-import Image from 'next/image';
-import { getPostContentFields } from 'helpers/getPostContentFields';
-import PostPageEmbededContent from './PostPageEmbededContent';
-import trimStringToLastSpace from 'helpers/trimStringToLastSpace';
-import Share from 'helpers/shareToSocialMedia';
+import React, { ReactElement } from "react";
+import styles from "../posts/ListStyles.module.css";
+import formateDate from "helpers/formateDate";
+import { generateImageUrl } from "helpers/imageUrlHelper";
+import Image from "next/image";
+import { getPostContentFields } from "helpers/getPostContentFields";
+import PostPageEmbededContent from "./PostPageEmbededContent";
+import trimStringToLastSpace from "helpers/trimStringToLastSpace";
+import Share from "helpers/shareToSocialMedia";
 
 export default function PostPageLegacyLayout({ post, locale }) {
   const { postTitle, postContent } = getPostContentFields(post, locale);
@@ -14,10 +14,13 @@ export default function PostPageLegacyLayout({ post, locale }) {
   let tagsDisplay: ReactElement[];
   if (post.tagNames && post.tagNames.length > 0) {
     let tagsArray = [post.tagNames];
-    if (post.tagNames.indexOf(',') > -1) tagsArray = post.tagNames.split(',');
+    if (post.tagNames.indexOf(",") > -1) tagsArray = post.tagNames.split(",");
     tagsDisplay = tagsArray.map((tag, index) => (
-      <a key={index} href={'/tag/' + tag}>
-        {' #' + tag}
+      <a
+        key={index}
+        href={"/tag/" + tag}
+      >
+        {" #" + tag}
       </a>
     ));
   }
@@ -27,7 +30,7 @@ export default function PostPageLegacyLayout({ post, locale }) {
       <div className={styles.legacyLayout}>
         <div className={styles.header}>
           <div className={styles.headerContainer}>
-            <p>Published {post.post_date ? formateDate(post.post_date) : ''}</p>
+            <p>Published {post.post_date ? formateDate(post.post_date) : ""}</p>
             <p>
               <a href={`/category/${post.categoryName}`}>
                 #{post.categoryName}
@@ -41,11 +44,19 @@ export default function PostPageLegacyLayout({ post, locale }) {
             <h2>{postTitle}</h2>
           </div>
         </div>
-
+        {post.post_image && post.post_image.indexOf("null") === -1 ? (
+          <Image
+            src={generateImageUrl(post.post_image)}
+            alt={post.post_title}
+            title={post.post_title}
+            layout="fill"
+            objectFit="cover"
+          />
+        ) : null}
         <div
           className={styles.content}
           dangerouslySetInnerHTML={{
-            __html: postContent.replace(/(?:\r\n|\r|\n)/g, '<br>'),
+            __html: postContent.replace(/(?:\r\n|\r|\n)/g, "<br>"),
           }}
         ></div>
       </div>
