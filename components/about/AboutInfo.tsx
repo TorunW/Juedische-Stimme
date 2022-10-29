a54938a79372571308b91b119fd35e46ecb6573b
david nelband <torun.wikstrom@gmail.com>
2022-10-29 14:24:37 +0200

    made board members gallery visible

--- components/about/AboutInfo.tsx
+++ components/about/AboutInfo.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'store/hooks';
-import Image from 'next/image';
-import Gallery from '../gallery/Gallery';
-import styles from './Styles.module.css';
-import containerBackground from 'styles/images/about.jpg';
-import { Container } from '../atoms/Container';
-import { generateImageUrl } from 'helpers/imageUrlHelper';
+import React, { useEffect, useState } from "react";
+import { useSelector } from "store/hooks";
+import Image from "next/image";
+import Gallery from "../gallery/Gallery";
+import styles from "./Styles.module.css";
+import containerBackground from "styles/images/about.jpg";
+import { Container } from "../atoms/Container";
+import { generateImageUrl } from "helpers/imageUrlHelper";
 
 const AboutInfo = ({ gallery, aboutInfo }) => {
   let [galleryImages, setGalleryImages] = React.useState([]);
@@ -23,32 +23,35 @@ const AboutInfo = ({ gallery, aboutInfo }) => {
   }
 
   return (
-    <section id='about-info' className={styles.aboutPage}>
+    <section
+      id="about-info"
+      className={styles.aboutPage}
+    >
       <h1>Über Uns</h1>
       <Container>
         {aboutInfo && aboutInfo !== null && (
           <div
-            data-testid='about-info-container'
+            data-testid="about-info-container"
             className={styles.aboutContainer}
           >
             <div
               dangerouslySetInnerHTML={{
                 __html:
-                  locale === 'en_US'
+                  locale === "en_US"
                     ? aboutInfo.text_top_en_US
                     : aboutInfo.text_top,
               }}
               className={styles.text}
             ></div>
             <div className={styles.menuContainer}>
-              <h3 className={styles.label}>Unsere partner {'&'} freunde</h3>
+              <h3 className={styles.label}>Unsere partner {"&"} freunde</h3>
               <div className={styles.imgContainer}>
                 {parterMenu.map((menuItem, index) => (
                   <a
                     key={menuItem.term_id + Date.now()}
                     href={menuItem.link}
-                    target='_blank'
-                    rel='noopener noreferrer'
+                    target="_blank"
+                    rel="noopener noreferrer"
                   >
                     <img
                       width={100}
@@ -61,20 +64,26 @@ const AboutInfo = ({ gallery, aboutInfo }) => {
               </div>
             </div>
 
-            {/* {galleryImages.length > 0 ? <Gallery images={galleryImages} /> : ''} */}
+            {galleryImages.length > 0 ? <Gallery images={galleryImages} /> : ""}
           </div>
         )}
       </Container>
       <div className={styles.contentContainer}>
-        <Image src={containerBackground} objectFit='cover' />
+        <Image
+          src={containerBackground}
+          objectFit="cover"
+        />
         <div className={styles.linkContainer}>
           <h3>Grundlagen unserer Arbeit sind:</h3>
           <div className={styles.btnContainer}>
-            <a href={'/selbstverstaendnis'} className={styles.linkButton}>
+            <a
+              href={"/selbstverstaendnis"}
+              className={styles.linkButton}
+            >
               Selbsverständnis der Jüdischen Stimme
             </a>
             <a
-              href={'/wp-content/uploads/2011/11/Satzung_2011.pdf'}
+              href={"/wp-content/uploads/2011/11/Satzung_2011.pdf"}
               className={styles.linkButton}
             >
               Satzung der Jüdischen Stimme
