1d2dbb9df565ffc771d8188393ad30ad987e8264
TorunW <torun.wikstrom@gmail.com>
2022-11-01 18:51:23 +0100

    memebership page responsive ui + delete unused files

--- components/facebook/FacebookFeed.tsx
+++ components/facebook/FacebookFeed.tsx
@@ -1,69 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-// import dateTimeHelper from 'helpers/dateTimeHelper';
-// import axios from 'axios';
-// import renderToString from 'helpers/renderToString';
-
-// import { setFeed } from 'store/fbdata/fbDataSlice';
-// import { useDispatch, useSelector } from 'store/hooks';
-
-// import { isUpdatedToday } from 'helpers/checkIfUpdatedToday';
 import { Container } from '../atoms/Container';
 
 const FacebookFeed = () => {
-  // const dispatch = useDispatch();
-  // const feed = useSelector((state) => state.fbData.feed);
-
-  // useEffect(() => {
-  //   fetchFacebookFeedFromDb();
-  // }, []);
-
-  // useEffect(() => {
-  //   if (fbt && fbt !== null) {
-  //     if (!feed || !isUpdatedToday(feed.date_updated))
-  //       fetchFacebookFeedFromFb();
-  //   }
-  // }, [fbt]);
-
-  // async function fetchFacebookFeedFromDb() {
-  //   const res = await fetch('/api/fbfeed');
-  //   const feedRes = await res.json();
-  //   dispatch(setFeed(feedRes[0]));
-  // }
-
-  // // each fbpost has a attachment.data => array, every data has a type, theres if its their photo its photo, if its a shared hen its smt else
-  // async function fetchFacebookFeedFromFb() {
-  //   const fields =
-  //     'id,likes.summary(true).limit(0),comments.summary(true).limit(0),reactions,shares,attachments,full_picture,message,from,permalink_url,created_time';
-  //   const fbFetchUrl = `https://graph.facebook.com/998665673528998/feed?limit=8&fields=${fields}&access_token=${fbt}`;
-  //   const res = await fetch(fbFetchUrl);
-  //   const fetchedFeed = await res.json();
-
-  //   // remove all the weird characters from the content to avoid mySql errors
-  //   if (fetchedFeed.data && fetchedFeed.data.length > 0) {
-  //     const renderedFeed = renderToString(fetchedFeed.data);
-  //     const data = {
-  //       content: renderedFeed,
-  //       date_updated: dateTimeHelper(new Date()),
-  //       type: 'posts',
-  //     };
-
-  //     axios({
-  //       method: 'post',
-  //       url: `/api/fbfeed`,
-  //       data,
-  //     }).then(
-  //       (response) => {
-  //         console.log(data, ' DATA ');
-  //         dispatch(setFeed(data));
-  //       },
-  //       (error) => {
-  //         console.log(error, 'ERROR on create fb feed record');
-  //       }
-  //     );
-  //   }
-  // }
-
   return (
     <Container sx={{ paddingX: '20px', textAlign: 'center' }}>
       <h2>Have a look at our social media</h2>
--- components/forms/MembershipForm.tsx
+++ components/forms/MembershipForm.tsx
@@ -1,10 +1,13 @@
-import React, { useEffect } from "react";
-import { useFormik } from "formik";
-import axios from "axios";
-import styles from "./Styles.module.css";
-import * as Yup from "yup";
-import { useSelector } from "store/hooks";
-import { getLabel } from "helpers/getLabelHelper";
+import React, { useEffect } from 'react';
+import { useFormik } from 'formik';
+import axios from 'axios';
+import styles from './Styles.module.css';
+import * as Yup from 'yup';
+import { useSelector } from 'store/hooks';
+import { getLabel } from 'helpers/getLabelHelper';
+import { TextField, Typography, useMediaQuery } from '@mui/material';
+import theme from 'config/theme';
+import Grid from '@mui/material/Grid';
 
 const MembershipForm = () => {
   const { labels } = useSelector((state) => state.labels);
@@ -14,62 +17,62 @@ const MembershipForm = () => {
 
   const formik = useFormik({
     initialValues: {
-      firstname: "",
-      lastname: "",
+      firstname: '',
+      lastname: '',
 
-      birthdateDay: "",
-      birthdateMonth: "",
-      birthdateYear: "",
+      birthdateDay: '',
+      birthdateMonth: '',
+      birthdateYear: '',
 
-      street: "",
-      streetNr: "",
-      zipcode: "",
-      city: "",
+      street: '',
+      streetNr: '',
+      zipcode: '',
+      city: '',
 
-      tel: "",
-      email: "",
+      tel: '',
+      email: '',
     },
     validationSchema: Yup.object().shape({
-      firstname: Yup.string().min(2, "* too short!").required("* required!"),
-      lastname: Yup.string().min(2, "* too short!").required("* required!"),
+      firstname: Yup.string().min(2, '* too short!').required('* required!'),
+      lastname: Yup.string().min(2, '* too short!').required('* required!'),
 
       birthdateDay: Yup.string()
         .required()
-        .matches(/^[0-9]+$/, "Must be only digits")
-        .min(2, "too short")
-        .max(2, "too long"),
+        .matches(/^[0-9]+$/, 'Must be only digits')
+        .min(2, 'too short')
+        .max(2, 'too long'),
       birthdateMonth: Yup.string()
         .required()
-        .matches(/^[0-9]+$/, "Must be only digits")
-        .min(2, "too short")
-        .max(2, "too long"),
+        .matches(/^[0-9]+$/, 'Must be only digits')
+        .min(2, 'too short')
+        .max(2, 'too long'),
       birthdateYear: Yup.string()
         .required()
-        .matches(/^[0-9]+$/, "Must be only digits")
-        .min(4, "too short")
-        .max(4, "too long"),
+        .matches(/^[0-9]+$/, 'Must be only digits')
+        .min(4, 'too short')
+        .max(4, 'too long'),
 
-      street: Yup.string().min(1, "* too short!").required("* required!"),
-      streetNr: Yup.number().min(1, "* too short!").required("* required!"),
-      zipcode: Yup.string().min(5, "* too short!").required("* required!"),
-      city: Yup.string().min(1, "* too short!").required("* required!"),
+      street: Yup.string().min(1, '* too short!').required('* required!'),
+      streetNr: Yup.number().min(1, '* too short!').required('* required!'),
+      zipcode: Yup.string().min(5, '* too short!').required('* required!'),
+      city: Yup.string().min(1, '* too short!').required('* required!'),
 
-      tel: Yup.number().min(7, "* too short!").required("* required!"),
-      email: Yup.string().email().required("* required!"),
+      tel: Yup.number().min(7, '* too short!').required('* required!'),
+      email: Yup.string().email().required('* required!'),
     }),
     onSubmit: (values) => {
       axios({
-        method: "post",
+        method: 'post',
         url: `/api/membership`,
         data: {
           ...values,
         },
       }).then(
         (response) => {
-          console.log(response, "did send form");
+          console.log(response, 'did send form');
         },
         (error) => {
-          console.log(error, "error");
+          console.log(error, 'error');
         }
       );
     },
@@ -78,192 +81,294 @@ const MembershipForm = () => {
   return (
     <div id='membership' className={styles.membershipForm}>
       <form onSubmit={formik.handleSubmit} className={styles.form}>
-        <h4>Dein Info Ausfüllen</h4>
-        <div className={styles.formRow}>
-          <input
-            id='firstname'
-            name='firstname'
-            type='text'
-            onChange={formik.handleChange}
-            value={formik.values.firstname}
-            placeholder={getLabel(labels, locale, 'firstname', 'Vorname')}
-          />
-          {formik.errors.firstname && formik.touched.firstname ? (
-            <span style={{ color: 'white' }}>{formik.errors.firstname}</span>
-          ) : (
-            ''
-          )}
-        </div>
-        <div className={styles.formRow}>
-          <input
-            id='lastname'
-            name='lastname'
-            type='text'
-            onChange={formik.handleChange}
-            value={formik.values.lastname}
-            placeholder={getLabel(labels, locale, 'lastname', 'Nachname')}
-          />
-          {formik.errors.lastname && formik.touched.lastname ? (
-            <span style={{ color: 'white' }}>{formik.errors.lastname}</span>
-          ) : (
-            ''
-          )}
-        </div>
+        <h3>Dein Info Ausfüllen</h3>
+        <Grid container spacing={isSmall ? 1 : 3}>
+          <Grid item xs={12}>
+            <TextField
+              id='firstname'
+              name='firstname'
+              type='text'
+              onChange={formik.handleChange}
+              value={formik.values.firstname}
+              placeholder={getLabel(labels, locale, 'firstname', 'Vorname')}
+              variant='standard'
+              color='secondary'
+              focused
+              sx={{
+                '& .MuiInputBase-root': {
+                  color: 'white',
+                },
+              }}
+              fullWidth
+            />
+            {formik.errors.firstname && formik.touched.firstname ? (
+              <span style={{ color: 'white' }}>{formik.errors.firstname}</span>
+            ) : (
+              ''
+            )}
+          </Grid>
 
-        <div className={styles.formRow}>
-          <div className={styles.formColumn}>
-            <input
+          <Grid item xs={12}>
+            <TextField
+              id='lastname'
+              name='lastname'
+              type='text'
+              onChange={formik.handleChange}
+              value={formik.values.lastname}
+              placeholder={getLabel(labels, locale, 'lastname', 'Nachname')}
+              variant='standard'
+              color='secondary'
+              focused
+              sx={{
+                '& .MuiInputBase-root': {
+                  color: 'white',
+                },
+              }}
+              fullWidth
+            />
+            {formik.errors.lastname && formik.touched.lastname ? (
+              <span style={{ color: 'white' }}>{formik.errors.lastname}</span>
+            ) : (
+              ''
+            )}
+          </Grid>
+
+          <Grid item xs={3}>
+            <TextField
               id='birthdateDay'
               name='birthdateDay'
               type='text'
               onChange={formik.handleChange}
               value={formik.values.birthdateDay}
               placeholder='TT'
+              variant='standard'
+              color='secondary'
+              focused
+              sx={{
+                '& .MuiInputBase-root': {
+                  color: 'white',
+                },
+              }}
+              fullWidth
             />
-          </div>
-          <span>/</span>
-          <div className={styles.formColumn}>
-            <input
+            {formik.errors.birthdateDay && formik.touched.birthdateDay ? (
+              <span style={{ color: 'white' }}>
+                {formik.errors.birthdateDay}
+              </span>
+            ) : (
+              ''
+            )}
+          </Grid>
+          <Grid item xs={1}>
+            <Typography sx={{ color: 'white', marginTop: 1 }}>/</Typography>
+          </Grid>
+          <Grid item xs={3}>
+            <TextField
               id='birthdateMonth'
               name='birthdateMonth'
               type='text'
               onChange={formik.handleChange}
               value={formik.values.birthdateMonth}
               placeholder='MM'
-            />{' '}
-          </div>
-          <span>/</span>
-          <div className={styles.formColumn}>
-            <input
+              variant='standard'
+              color='secondary'
+              focused
+              sx={{
+                '& .MuiInputBase-root': {
+                  color: 'white',
+                },
+              }}
+              fullWidth
+            />
+            {formik.errors.birthdateMonth && formik.touched.birthdateMonth ? (
+              <span style={{ color: 'white' }}>
+                {formik.errors.birthdateMonth}
+              </span>
+            ) : (
+              ''
+            )}{' '}
+          </Grid>
+          <Grid item xs={1}>
+            <Typography sx={{ color: 'white', marginTop: 1 }}>/</Typography>
+          </Grid>
+          <Grid item xs={3}>
+            <TextField
               id='birthdateYear'
               name='birthdateYear'
               type='text'
               onChange={formik.handleChange}
               value={formik.values.birthdateYear}
               placeholder='YYYY'
-            />{' '}
-          </div>
-          {formik.errors.birthdateDay && formik.touched.birthdateDay ? (
-            <span style={{ color: "white" }}>{formik.errors.birthdateDay}</span>
-          ) : (
-            ""
-          )}
-          {formik.errors.birthdateMonth && formik.touched.birthdateMonth ? (
-            <span style={{ color: "white" }}>
-              {formik.errors.birthdateMonth}
-            </span>
-          ) : (
-            ""
-          )}
-          {formik.errors.birthdateYear && formik.touched.birthdateYear ? (
-            <span style={{ color: "white" }}>
-              {formik.errors.birthdateYear}
-            </span>
-          ) : (
-            ""
-          )}
-        </div>
+              variant='standard'
+              color='secondary'
+              focused
+              sx={{
+                '& .MuiInputBase-root': {
+                  color: 'white',
+                },
+              }}
+              fullWidth
+            />
+            {formik.errors.birthdateYear && formik.touched.birthdateYear ? (
+              <span style={{ color: 'white' }}>
+                {formik.errors.birthdateYear}
+              </span>
+            ) : (
+              ''
+            )}
+          </Grid>
 
-        <div className={styles.formRow}>
-          <div className={styles.formColumn}>
-            <input
+          <Grid item xs={9}>
+            <TextField
               id='street'
               name='street'
               type='text'
               onChange={formik.handleChange}
               value={formik.values.street}
               placeholder='Straße'
+              variant='standard'
+              color='secondary'
+              focused
+              sx={{
+                '& .MuiInputBase-root': {
+                  color: 'white',
+                },
+              }}
+              fullWidth
             />
             {formik.errors.street && formik.touched.street ? (
-              <span style={{ color: "white" }}>{formik.errors.street}</span>
+              <span style={{ color: 'white' }}>{formik.errors.street}</span>
             ) : (
-              ""
+              ''
             )}
-          </div>
-
-          <div className={styles.formColumn}>
-            <input
+          </Grid>
+          <Grid item xs={3}>
+            <TextField
               id='streetNr'
               name='streetNr'
               type='text'
               onChange={formik.handleChange}
               value={formik.values.streetNr}
               placeholder='Nr'
+              variant='standard'
+              color='secondary'
+              focused
+              sx={{
+                '& .MuiInputBase-root': {
+                  color: 'white',
+                },
+              }}
+              fullWidth
             />
             {formik.errors.streetNr && formik.touched.streetNr ? (
-              <span style={{ color: "white" }}>{formik.errors.streetNr}</span>
+              <span style={{ color: 'white' }}>{formik.errors.streetNr}</span>
             ) : (
-              ""
+              ''
             )}
           </Grid>
 
-        <div className={styles.formRow}>
-          <div className={styles.formColumn}>
-            <input
+          <Grid item xs={5}>
+            <TextField
               id='zipcode'
               name='zipcode'
               type='text'
               onChange={formik.handleChange}
               value={formik.values.zipcode}
               placeholder='PLZ'
+              variant='standard'
+              color='secondary'
+              focused
+              sx={{
+                '& .MuiInputBase-root': {
+                  color: 'white',
+                },
+              }}
+              fullWidth
             />
             {formik.errors.zipcode && formik.touched.zipcode ? (
               <span style={{ color: 'white' }}>{formik.errors.zipcode}</span>
             ) : (
               ''
             )}
-          </div>
-          <div className={styles.formColumn}>
-            <input
+          </Grid>
+          <Grid item xs={7}>
+            <TextField
               id='city'
               name='city'
               type='text'
               onChange={formik.handleChange}
               value={formik.values.city}
               placeholder='City'
+              variant='standard'
+              color='secondary'
+              focused
+              sx={{
+                '& .MuiInputBase-root': {
+                  color: 'white',
+                },
+              }}
+              fullWidth
             />
             {formik.errors.city && formik.touched.city ? (
               <span style={{ color: 'white' }}>{formik.errors.city}</span>
             ) : (
               ''
             )}
-          </div>
-        </div>
-
-        <div className={styles.formRow}>
-          <input
-            id='tel'
-            name='tel'
-            type='text'
-            onChange={formik.handleChange}
-            value={formik.values.tel}
-            placeholder='Tel'
-          />
-          {formik.errors.tel && formik.touched.tel ? (
-            <span style={{ color: "white" }}>{formik.errors.tel}</span>
-          ) : (
-            ""
-          )}
-
-        <div className={styles.formRow}>
-          <input
-            id='email'
-            name='email'
-            type='email'
-            onChange={formik.handleChange}
-            value={formik.values.email}
-            placeholder='Email'
-          />
-          {formik.errors.email && formik.touched.email ? (
-            <span style={{ color: "white" }}>{formik.errors.email}</span>
-          ) : (
-            ""
-          )}
-        </div>
-
-        <div className='button blackBg submitBtn'>
-          <button type='submit'>Senden</button>
-        </div>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              id='tel'
+              name='tel'
+              type='text'
+              onChange={formik.handleChange}
+              value={formik.values.tel}
+              placeholder='Tel'
+              variant='standard'
+              color='secondary'
+              focused
+              sx={{
+                '& .MuiInputBase-root': {
+                  color: 'white',
+                },
+              }}
+              fullWidth
+            />
+            {formik.errors.tel && formik.touched.tel ? (
+              <span style={{ color: 'white' }}>{formik.errors.tel}</span>
+            ) : (
+              ''
+            )}
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              id='email'
+              name='email'
+              type='email'
+              onChange={formik.handleChange}
+              value={formik.values.email}
+              placeholder='Email'
+              variant='standard'
+              color='secondary'
+              focused
+              sx={{
+                '& .MuiInputBase-root': {
+                  color: 'white',
+                },
+              }}
+              fullWidth
+            />
+            {formik.errors.email && formik.touched.email ? (
+              <span style={{ color: 'white' }}>{formik.errors.email}</span>
+            ) : (
+              ''
+            )}
+          </Grid>
+          <Grid item xs={12} marginTop={2}>
+            <div className='button blackBg submitBtn'>
+              <button type='submit'>Senden</button>
+            </div>
+          </Grid>
+        </Grid>
       </form>
     </div>
   );
