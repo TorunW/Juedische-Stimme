eec28f50f1a70ebe867d2ba8e333fadf352d0154
david nelband <torun.wikstrom@gmail.com>
2022-11-13 22:41:50 +0100

    added margin to footer menu

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import styles from './Styles.module.css';
-import ContactForm from '../forms/ContactForm';
-import { useSelector } from 'store/hooks';
-import { Container } from '../atoms/Container';
+import React from "react";
+import styles from "./Styles.module.css";
+import ContactForm from "../forms/ContactForm";
+import { useSelector } from "store/hooks";
+import { Container } from "../atoms/Container";
+import { Box } from "@mui/system";
 
 export const Footer = () => {
   const { footerMenu } = useSelector((state) => state.nav);
@@ -14,18 +15,19 @@ export const Footer = () => {
           <ContactForm />
         </div>
         <div className={styles.bottomRow}>
-          <p>
+          <Box marginBottom={3}>
             copyright © 2022 JÜDISCHE STIMME für gerechten frieden in nahost,
             berlin seit 2007.
-            <br />
+          </Box>
+          <p>
             {footerMenu.map((mi, index) => (
               <React.Fragment key={mi.title}>
                 <a href={mi.link}>{mi.title}</a>
 
                 {index + 1 < footerMenu.length ? (
-                  <span style={{ margin: '0 5px' }}>●</span>
+                  <span style={{ margin: "0 5px" }}>●</span>
                 ) : (
-                  ''
+                  ""
                 )}
               </React.Fragment>
             ))}
