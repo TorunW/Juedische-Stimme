cf23342304331c09ae3b5a235650affc09624506
david nelband <torun.wikstrom@gmail.com>
2022-11-14 20:24:36 +0100

    admin nav user pill refactor

--- components/admin/Nav.tsx
+++ components/admin/Nav.tsx
@@ -11,6 +11,7 @@ import {
   Typography,
   Button,
   ButtonGroup,
+  Stack,
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
@@ -47,218 +48,223 @@ function AdminNav() {
   }
 
   return (
-    <Box sx={{ height: "100%" }}>
-      <Box sx={{ textAlign: "center", marginY: 2 }}>
-        <Link href={"/admin"}>
-          <Typography variant="h5">Admin Menu</Typography>
-        </Link>
-      </Box>
-      <Divider />
-      <Accordion
-        disableGutters
-        elevation={0}
-      >
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <ArticleIcon sx={{ marginRight: 1 }} />
-          <Typography>Posts</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Divider />
-          <Box sx={{ marginY: 1, marginLeft: 1 }}>
-            <Link href={`/admin/posts/create`}>
-              <Box
-                display="flex"
-                sx={{ cursor: "pointer" }}
-              >
-                <NoteAddIcon sx={{ marginRight: 1 }} />
-                <Typography>Create new post</Typography>
-              </Box>
-            </Link>
-          </Box>
-          <Divider />
-          <Box sx={{ marginY: 1, marginLeft: 1 }}>
-            <Link href={`/admin/posts`}>
-              <Box
-                display="flex"
-                sx={{ cursor: "pointer" }}
-              >
-                <EditIcon sx={{ marginRight: 1 }} />
-                <Typography>Edit posts</Typography>
-              </Box>
-            </Link>
-          </Box>
+    <Stack
+      justifyContent="space-between"
+      height="100vh"
+    >
+      <Stack>
+        <Box sx={{ textAlign: "center", marginY: 2 }}>
+          <Link href={"/admin"}>
+            <Typography variant="h5">Admin Menu</Typography>
+          </Link>
+        </Box>
+        <Divider />
+        <Accordion
+          disableGutters
+          elevation={0}
+        >
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <ArticleIcon sx={{ marginRight: 1 }} />
+            <Typography>Posts</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Divider />
+            <Box sx={{ marginY: 1, marginLeft: 1 }}>
+              <Link href={`/admin/posts/create`}>
+                <Box
+                  display="flex"
+                  sx={{ cursor: "pointer" }}
+                >
+                  <NoteAddIcon sx={{ marginRight: 1 }} />
+                  <Typography>Create new post</Typography>
+                </Box>
+              </Link>
+            </Box>
+            <Divider />
+            <Box sx={{ marginY: 1, marginLeft: 1 }}>
+              <Link href={`/admin/posts`}>
+                <Box
+                  display="flex"
+                  sx={{ cursor: "pointer" }}
+                >
+                  <EditIcon sx={{ marginRight: 1 }} />
+                  <Typography>Edit posts</Typography>
+                </Box>
+              </Link>
+            </Box>
 
-          <Divider />
-          <Box sx={{ marginY: 1, marginLeft: 1 }}>
-            <Link href={`/admin/tags`}>
-              <Box
-                display="flex"
-                sx={{ cursor: "pointer" }}
-              >
-                <TagIcon sx={{ marginRight: 1 }} />
-                <Typography>Tags</Typography>
-              </Box>
-            </Link>
-          </Box>
+            <Divider />
+            <Box sx={{ marginY: 1, marginLeft: 1 }}>
+              <Link href={`/admin/tags`}>
+                <Box
+                  display="flex"
+                  sx={{ cursor: "pointer" }}
+                >
+                  <TagIcon sx={{ marginRight: 1 }} />
+                  <Typography>Tags</Typography>
+                </Box>
+              </Link>
+            </Box>
 
-          <Divider />
-          <Box sx={{ marginY: 1, marginLeft: 1 }}>
-            <Link href={`/admin/categories`}>
-              <Box
-                display="flex"
-                sx={{ cursor: "pointer" }}
-              >
-                <CategoryIcon sx={{ marginRight: 1 }} />
-                <Typography>Categories</Typography>
-              </Box>
-            </Link>
-          </Box>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion
-        disableGutters
-        elevation={0}
-      >
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <WebIcon sx={{ marginRight: 1 }} />
-          <Typography>Sections</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Divider />
-          <Box sx={{ marginY: 1, marginLeft: 1 }}>
-            <Link href={`/admin/about`}>
-              <Box
-                display="flex"
-                sx={{ cursor: "pointer" }}
-              >
-                <InfoIcon sx={{ marginRight: 1 }} />
-                <Typography>About</Typography>
-              </Box>
-            </Link>
-          </Box>
-          <Divider />
-          <Box sx={{ marginY: 1, marginLeft: 1 }}>
-            <a href={`/admin/posts/spenden`}>
-              <Box
-                display="flex"
-                sx={{ cursor: "pointer" }}
-              >
-                <VolunteerActivismIcon sx={{ marginRight: 1 }} />
-                <Typography>Donations</Typography>
-              </Box>
-            </a>
-          </Box>
-          <Divider />
-          <Box sx={{ marginY: 1, marginLeft: 1 }}>
-            <a href={`/admin/posts/mitgliedsantrag`}>
-              <Box
-                display="flex"
-                sx={{ cursor: "pointer" }}
-              >
-                <CardMembershipIcon sx={{ marginRight: 1 }} />
-                <Typography>Membership</Typography>
-              </Box>
-            </a>
-          </Box>
-          <Divider />
-          <Box sx={{ marginY: 1, marginLeft: 1 }}>
-            <Link href={`/admin/header`}>
-              <Box
-                display="flex"
-                sx={{ cursor: "pointer" }}
-              >
-                <ViewCarouselIcon sx={{ marginRight: 1 }} />
-                <Typography>Header</Typography>
-              </Box>
-            </Link>
-          </Box>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion
-        disableGutters
-        elevation={0}
-      >
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <MenuIcon sx={{ marginRight: 1 }} />
-          <Typography>Menus</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Divider />
-          <Box sx={{ marginY: 1, marginLeft: 1 }}>
-            <Link href={`/admin/menus`}>
-              <Box
-                display="flex"
-                sx={{ cursor: "pointer" }}
-              >
-                <ListIcon sx={{ marginRight: 1 }} />
-                <Typography>Menus & Links</Typography>
-              </Box>
-            </Link>
-          </Box>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion
-        disableGutters
-        elevation={0}
-      >
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <GroupIcon sx={{ marginRight: 1 }} />
-          <Typography>Users</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Divider />
+            <Divider />
+            <Box sx={{ marginY: 1, marginLeft: 1 }}>
+              <Link href={`/admin/categories`}>
+                <Box
+                  display="flex"
+                  sx={{ cursor: "pointer" }}
+                >
+                  <CategoryIcon sx={{ marginRight: 1 }} />
+                  <Typography>Categories</Typography>
+                </Box>
+              </Link>
+            </Box>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion
+          disableGutters
+          elevation={0}
+        >
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <WebIcon sx={{ marginRight: 1 }} />
+            <Typography>Sections</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Divider />
+            <Box sx={{ marginY: 1, marginLeft: 1 }}>
+              <Link href={`/admin/about`}>
+                <Box
+                  display="flex"
+                  sx={{ cursor: "pointer" }}
+                >
+                  <InfoIcon sx={{ marginRight: 1 }} />
+                  <Typography>About</Typography>
+                </Box>
+              </Link>
+            </Box>
+            <Divider />
+            <Box sx={{ marginY: 1, marginLeft: 1 }}>
+              <a href={`/admin/posts/spenden`}>
+                <Box
+                  display="flex"
+                  sx={{ cursor: "pointer" }}
+                >
+                  <VolunteerActivismIcon sx={{ marginRight: 1 }} />
+                  <Typography>Donations</Typography>
+                </Box>
+              </a>
+            </Box>
+            <Divider />
+            <Box sx={{ marginY: 1, marginLeft: 1 }}>
+              <a href={`/admin/posts/mitgliedsantrag`}>
+                <Box
+                  display="flex"
+                  sx={{ cursor: "pointer" }}
+                >
+                  <CardMembershipIcon sx={{ marginRight: 1 }} />
+                  <Typography>Membership</Typography>
+                </Box>
+              </a>
+            </Box>
+            <Divider />
+            <Box sx={{ marginY: 1, marginLeft: 1 }}>
+              <Link href={`/admin/header`}>
+                <Box
+                  display="flex"
+                  sx={{ cursor: "pointer" }}
+                >
+                  <ViewCarouselIcon sx={{ marginRight: 1 }} />
+                  <Typography>Header</Typography>
+                </Box>
+              </Link>
+            </Box>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion
+          disableGutters
+          elevation={0}
+        >
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <MenuIcon sx={{ marginRight: 1 }} />
+            <Typography>Menus</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Divider />
+            <Box sx={{ marginY: 1, marginLeft: 1 }}>
+              <Link href={`/admin/menus`}>
+                <Box
+                  display="flex"
+                  sx={{ cursor: "pointer" }}
+                >
+                  <ListIcon sx={{ marginRight: 1 }} />
+                  <Typography>Menus & Links</Typography>
+                </Box>
+              </Link>
+            </Box>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion
+          disableGutters
+          elevation={0}
+        >
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <GroupIcon sx={{ marginRight: 1 }} />
+            <Typography>Users</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Divider />
 
-          <Box sx={{ marginY: 1, marginLeft: 1 }}>
-            <Link href={`/admin/users/register`}>
-              <Box
-                display="flex"
-                sx={{ cursor: "pointer" }}
-              >
-                <GroupAddIcon sx={{ marginRight: 1 }} />
-                <Typography>Register User</Typography>
-              </Box>
-            </Link>
-          </Box>
-          <Divider />
-          <Box sx={{ marginY: 1, marginLeft: 1 }}>
-            <Link href={`/admin/users`}>
-              <Box
-                display="flex"
-                sx={{ cursor: "pointer" }}
-              >
-                <PeopleOutlineIcon sx={{ marginRight: 1 }} />
-                <Typography>Edit Users</Typography>
-              </Box>
-            </Link>
-          </Box>
-        </AccordionDetails>
-      </Accordion>
-      <Divider />
-      <Box sx={{ marginY: 1, marginLeft: 1, paddingLeft: 1 }}>
-        <Link href={`/admin/labels`}>
-          <Box
-            display="flex"
-            sx={{ cursor: "pointer" }}
-          >
-            <LabelIcon sx={{ marginRight: 1 }} />
-            <Typography>Labels</Typography>
-          </Box>
-        </Link>
-      </Box>
-      <Divider />
-      <Box sx={{ marginY: 1, marginLeft: 1, paddingLeft: 1 }}>
-        <Link href={`/admin/media`}>
-          <Box
-            display="flex"
-            sx={{ cursor: "pointer" }}
-          >
-            <ImageSearchIcon sx={{ marginRight: 1 }} />
-            <Typography>Media Items</Typography>
-          </Box>
-        </Link>
-      </Box>
-      <Divider />
-      <Box sx={{ textAlign: "center", marginTop: 2 }}>
+            <Box sx={{ marginY: 1, marginLeft: 1 }}>
+              <Link href={`/admin/users/register`}>
+                <Box
+                  display="flex"
+                  sx={{ cursor: "pointer" }}
+                >
+                  <GroupAddIcon sx={{ marginRight: 1 }} />
+                  <Typography>Register User</Typography>
+                </Box>
+              </Link>
+            </Box>
+            <Divider />
+            <Box sx={{ marginY: 1, marginLeft: 1 }}>
+              <Link href={`/admin/users`}>
+                <Box
+                  display="flex"
+                  sx={{ cursor: "pointer" }}
+                >
+                  <PeopleOutlineIcon sx={{ marginRight: 1 }} />
+                  <Typography>Edit Users</Typography>
+                </Box>
+              </Link>
+            </Box>
+          </AccordionDetails>
+        </Accordion>
+        <Divider />
+        <Box sx={{ marginY: 1, marginLeft: 1, paddingLeft: 1 }}>
+          <Link href={`/admin/labels`}>
+            <Box
+              display="flex"
+              sx={{ cursor: "pointer" }}
+            >
+              <LabelIcon sx={{ marginRight: 1 }} />
+              <Typography>Labels</Typography>
+            </Box>
+          </Link>
+        </Box>
+        <Divider />
+        <Box sx={{ marginY: 1, marginLeft: 1, paddingLeft: 1 }}>
+          <Link href={`/admin/media`}>
+            <Box
+              display="flex"
+              sx={{ cursor: "pointer" }}
+            >
+              <ImageSearchIcon sx={{ marginRight: 1 }} />
+              <Typography>Media Items</Typography>
+            </Box>
+          </Link>
+        </Box>
+        <Divider />
+      </Stack>
+      <Box sx={{ textAlign: "left", p: 1 }}>
         <ButtonGroup
           variant="contained"
           color="secondary"
@@ -275,11 +281,16 @@ function AdminNav() {
             onClick={() => router.push(`/admin/users/${loggedUser?.ID}`)}
           >
             <AccountCircleIcon sx={{ marginRight: 1 }} />
-            {!!loggedUser
-              ? loggedUser.display_name?.length > 0
-                ? " " + loggedUser.display_name
-                : " " + loggedUser.user_email
-              : ""}
+            <Typography
+              fontSize="15px"
+              width="135px"
+              textAlign="left"
+              textTransform="none"
+            >
+              {loggedUser?.display_name?.length > 0
+                ? " " + loggedUser?.display_name
+                : " " + loggedUser?.user_email}
+            </Typography>
           </Button>
           <Button
             onClick={logout}
@@ -293,7 +304,7 @@ function AdminNav() {
           </Button>
         </ButtonGroup>
       </Box>
-    </Box>
+    </Stack>
   );
 }
 
