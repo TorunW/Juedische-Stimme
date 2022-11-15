86d8090b5e2332254579f5c82bd59b1cd12012e6
david nelband <torun.wikstrom@gmail.com>
2022-11-15 17:04:07 +0100

    oimport

--- pages/[name]/index.tsx
+++ pages/[name]/index.tsx
@@ -1,19 +1,18 @@
-import { ReactElement, useEffect } from "react";
-import excuteQuery from "lib/db";
-import { useDispatch } from "react-redux";
-import Head from "next/head";
-import { selectPostByName } from "lib/queries/posts";
-import { setMenuItems } from "store/nav/navSlice";
-import { LayoutPage } from "types/LayoutPage.type";
-import { LayoutPageProps } from "types/LayoutPageProps.type";
-import { setLanguages } from "store/languages/languagesSlice";
-import { setPost } from "store/posts/postsSlice";
 import PostPage from "components/posts/PostPage";
+import { postNameToString } from "helpers/postNameToString";
+import excuteQuery from "lib/db";
+import { selectPostByName } from "lib/queries/posts";
+import Head from "next/head";
 import { createServerSideProps } from "page/server-side-props";
 import { HomePageProps } from "pages";
-import { useSelector } from "store/hooks";
+import { ReactElement, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { setLabels } from "store/labels/labelsSlice";
-import { postNameToString } from "helpers/postNameToString";
+import { setLanguages } from "store/languages/languagesSlice";
+import { setMenuItems } from "store/nav/navSlice";
+import { setPost } from "store/posts/postsSlice";
+import { LayoutPage } from "types/LayoutPage.type";
+import { LayoutPageProps } from "types/LayoutPageProps.type";
 
 export const getServerSideProps = createServerSideProps<HomePageProps>(
   async ({ context, data: { navItems, labels, locale } }) => {
